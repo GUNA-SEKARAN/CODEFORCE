2ce29d9177cd87ce955b9f3934954612f4c68dcf
GUNA SEKARAN <88916677+GUNA-SEKARAN@users.noreply.github.com>
2021-08-16 19:43:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrong_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = Console.ReadLine();
+            string[] ans = input.Split();
+            int n = Convert.ToInt32(ans[0]);
+            int k = Convert.ToInt32(ans[1]);
+            for (int i = 0; i < k; i++)
+            {
+              
+                
+                if (n % 10 == 0)
+                {
+                    n = n / 10;
+                }
+                else
+                { 
+                n--;
+                }
+                Console.WriteLine(n);
+               
+
+            }
+          
+
+        }
+    }
+}
